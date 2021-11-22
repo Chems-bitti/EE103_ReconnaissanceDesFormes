20617b604ec5c6440ab07cd7ef8d53ced99670bf
Benjyapa <94632472+Benjyapa@users.noreply.github.com>
2021-11-22 17:02:45 +0100

    Create calcul.h

---
+++ calcul.h
@@ -0,0 +1,27 @@
+#ifndef _calcul_h_
+#define _calcul_h_
+
+// Bibliotheques "standards"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// déclaration de la structure Matrice
+typedef struct {
+    int numero;         // numéro de la matrice pour l'identidié
+    int DIMx, DIMy;     // dimention de la matrice en x et en y
+    double **tab        // tableau avec les valeurs de la matrice
+} Matrice;
+
+
+
+// Methodes de parcours de liste
+/** Calcule la distance euclidienne entre deux matrice de moment
+  * @param mat1 la premier matrice
+  * @param mat2 la deuxieme matrice
+  * @return La distance euclidienne des deux matrices
+  */
+double distance_euclidienne(Matrice mat1, Matrice mat2);
+
+
+#endif
