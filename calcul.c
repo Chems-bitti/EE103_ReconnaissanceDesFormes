544f1d4071f51eb9999a8dae36fcc2d769c8077a
Azeyro <62699968+Azeyro@users.noreply.github.com>
2021-11-22 17:11:51 +0100

    Update calcul.c

--- calcul.c
+++ calcul.c
@@ -1,6 +1,29 @@
 #include "calcul.h"
 
-double distance_euclidienne(double **a1, double **a2, int dimx, int dimy){
+double distance_euclidienne(Matrice mat1, Matrice mat2){
+
+    int i,j;
+    double res= 0;
+    
+    if (mat1.DIMx != mat2.DIMx || mat1.DIMy != mat2.DIMy){
+        printf("ERREUR : Les matrices ne sont pas de meme dimension\n");
+        return 0;
+    }else{
+        for(i = 0 ; i < mat1.DIMx; i++){
+            for(j = 0 ; j < mat1.DIMy; j++){
+                res = res + (mat1.tab[i][j]-a2[i][j])*(a1[i][j]-a2[i][j]);
+            }
+        }
+    }
+    res = sqrt(res);
+
+    return res;
+
+}
+
+
+
+/*double distance_euclidienne(double **a1, double **a2, int dimx, int dimy){
 
     int i,j;
     double res= 0;
@@ -14,5 +37,4 @@ double distance_euclidienne(double **a1, double **a2, int dimx, int dimy){
 
     return res;
 
-}
-
+}*/
