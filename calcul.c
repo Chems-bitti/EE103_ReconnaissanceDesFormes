2e30a7143f56a1a3390ad712e4e29723a65575f4
Benjyapa <94632472+Benjyapa@users.noreply.github.com>
2021-11-22 18:27:42 +0100

    Update calcul.c

--- calcul.c
+++ calcul.c
@@ -5,20 +5,17 @@ double distance_euclidienne(Matrice mat1, Matrice mat2){
     int i,j;
     double res= 0;
     
-    if (mat1.DIMx != mat2.DIMx || mat1.DIMy != mat2.DIMy){
-        printf("ERREUR : Les matrices ne sont pas de meme dimension\n");
+    if mat1.N != mat2.N{
+        printf("ERREUR : Les matrices ne sont pas de meme ordre\n");
         return 0;
     }else{
-        for(i = 0 ; i < mat1.DIMx; i++){
-            for(j = 0 ; j < mat1.DIMy; j++){
-                res = res + (mat1.tab[i][j]-a2[i][j])*(a1[i][j]-a2[i][j]);
+        for(i = 0 ; i < mat1.N; i++){
+            for(j = 0 ; j < mat1.N; j++){
+                res += pow(mat1.tab[i][j]-mat2.tab[i][j],2);
             }
         }
     }
-    res = sqrt(res);
-
-    return res;
-
+    return sqrt(res);
 }
 
 
@@ -28,8 +25,8 @@ unsigned char reconstruction_pixel(int x, int y, Matrice mat){
     unsigned char somme1 = 0 ;
     unsigned char somme2 = 0;
 
-    for(p = 0; p < N; p++){
-        for (q = 0; q < N-p; q++){
+    for(p = 0; p < mat.N; p++){
+        for (q = 0; q < mat.N-p; q++){
             somme1 += mat.tab[p][q] * base_legendre(x,p) * base_legendre(y,q);
         }
         somme2 += somme1;
