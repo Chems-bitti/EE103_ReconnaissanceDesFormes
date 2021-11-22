bf6c5fb256f8200a6de99ae6c48ddbd9f02663fb
Azeyro <62699968+Azeyro@users.noreply.github.com>
2021-11-22 16:39:54 +0100

    Create calcul.c

---
+++ calcul.c
@@ -0,0 +1,16 @@
+double distance_euclidienne(double **a1, double **a2, int dimx, int dimy){
+
+    int i,j;
+    double res= 0;
+
+    for(i = 0 ; i < dimx; i++){
+        for(j = 0 ; j < dimy ; j++){
+            res = res + (a1[i][j]-a2[i][j])*(a1[i][j]-a2[i][j]);
+        }
+    }
+    res = sqrt(res);
+
+    return res;
+
+}
+
