c8e1cbbf7f37b4eae9e64252c21b3edfd1158b69
Benjyapa <94632472+Benjyapa@users.noreply.github.com>
2021-11-22 18:31:52 +0100

    Update calcul.h

--- calcul.h
+++ calcul.h
@@ -14,14 +14,20 @@ typedef struct {
 } Matrice;
 
 
-
-// Methodes de parcours de liste
 /** Calcule la distance euclidienne entre deux matrice de moment
   * @param mat1 la premier matrice
   * @param mat2 la deuxieme matrice
   * @return La distance euclidienne des deux matrices
   */
 double distance_euclidienne(Matrice mat1, Matrice mat2);
+
+/** Reconstruit un pixel de coordonnee (x,y) à partir du Matrice de moment de Legendre
+  * @param x coordonnee en abscisse
+  * @param y coordonnee en ordonnee
+  * @param mat Matrice de moment de Legendre
+  * @return La valeur du pixel
+  */
+unsigned char reconstruction_pixel(int x, int y, Matrice mat);
 
 double base_legendre(int x, int n);
 
