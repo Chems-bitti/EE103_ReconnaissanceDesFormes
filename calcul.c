d42a6f94db4fc627b60ef3206034c64a06fdd1e3
Duael01 <46139320+Duael01@users.noreply.github.com>
2021-11-29 16:47:39 +0100

    Update calcul.c

--- calcul.c
+++ calcul.c
@@ -9,7 +9,8 @@ double base_legendre(int x, int n){
         an.tab[1][0]=0;
         an.tab[1][1]=1;
     }
-    for(int i=2; i<=n; i++){    //Détermination de tout les an jusqu'à l'ordre souhaité
+
+    for(int i=2; i<=n; i++){    //Détermination de tous les an jusqu'à l'ordre souhaité
         for(int j=0; j<=i; j++){
             if(j==0){
                 an.tab[i][j]=-(i-1)*(an.tab[i-2][j])/i;
@@ -24,10 +25,10 @@ double base_legendre(int x, int n){
     }
 
     for(int i=0; i<=n; i++){
-        res+=an.tab[n][i]*pow(x,i);
+        res+=(an.tab[n][i])*pow(x,i);
     }
-
     supprMatrice(&an);
 
     return res;
 }
+
