4d5980a4ffb464808959aff7b38cd6e931c30807
Benjyapa <94632472+Benjyapa@users.noreply.github.com>
2021-11-29 15:33:58 +0100

    Add files via upload

--- gestionfichier.c
+++ gestionfichier.c
@@ -3,31 +3,29 @@
 
 int ecritureMatrice(char * nomfichier, Matrice mat, int TypeEcriture){
 
-
     FILE *ftxt = fopen( nomfichier, "a" );
     return 0;
 
 }
 
 
-int ecritureMatricebin(char * nomfichierbin, Matrice mat, int TypeEcriture){
+int ecritureMatricebin(char *nomfichierbin, Matrice mat, int TypeEcriture){
 
-    // Choisi le bon type d'ouverture du fichier
-    if TypeEcriture{
-        FILE *fbin = fopen(nomfichierbin, "wb");
-    }else{
+    //if (TypeEcriture){
         FILE *fbin = fopen(nomfichierbin, "ab");
-    }
+    //}else{
+        //FILE *fbin = fopen(nomfichierbin, "wb");
+    //}
 
     // Controle si le fichier s'est bien ouvert
-    if fbin == NULL{
+    if (fbin == NULL){
         printf("Erreur d'ouverture: %s \n",nomfichierbin);
         return 0;
     }else{
 
-        fwrite ( &mat.num , sizeof (int) , 1 , fBin );                  // Ecrit le numero de la matrice
-        fwrite ( &mat.N , sizeof (int) , 1 , fBin );                    // Ecrit l'ordre de la matrice
-        fwrite ( &mat.tab , sizeof (double) , pow(mat.N,2) , fBin );    // Ecrit la matrice
+        fwrite ( &mat.num , sizeof (int) , 1 , fbin );                  // Ecrit le numero de la matrice
+        fwrite ( &mat.N , sizeof (int) , 1 , fbin );                    // Ecrit l'ordre de la matrice
+        fwrite ( &mat.tab , sizeof (double) , pow(mat.N,2) , fbin );    // Ecrit la matrice
 
         // Fermeture du fichier
         fclose(fbin);
--- gestionfichier.h
+++ gestionfichier.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include "matrice.h"
 
+// Pas creer !!
 /** Ecrit une matrice dans un fichier texte
   * 1ere ligne : numero de matrice - tab - ordre de la matrice
   * ligne suivant : valeur de la matrice place en triangulaire inférieure pour ne pas avoir a marquer les 0
@@ -17,9 +18,15 @@
   */
 int ecritureMatrice(char * nomfichier, Matrice mat, int TypeEcriture);
 
-//Matrice lectureMatrice(char * nomfichier, int num);
+// Pas creer !!
+/** Recupere une matrice dans un fichier texte
+  * @param nomfichier Le nom du fichier dans lequel lire la matrice
+  * @param num numero de la matrice
+  * @return la matrice
+  */
+Matrice lectureMatrice(char * nomfichier, int num);
 
-
+// Pas creer !!
 /** Ecrit une matrice dans un fichier binaire
   * Les deux premiers entier sont pour le numéro et l'ordre de la matrice, le tableau en deux dimension pour les valeurs
   * @param nomfichierbin Le nom du fichier dans lequel ecrire la matrice
@@ -29,7 +36,12 @@ int ecritureMatrice(char * nomfichier, Matrice mat, int TypeEcriture);
   */
 int ecritureMatricebin(char * nomfichierbin, Matrice mat, int TypeEcriture);
 
-
+// Pas creer !!
+/** Recupere une matrice dans un fichier binaire
+  * @param nomfichier Le nom du fichier dans lequel lire la matrice
+  * @param num numero de la matrice
+  * @return la matrice
+  */
 Matrice lectureMatricebin(char * nomfichierbin, int num);
 
 
