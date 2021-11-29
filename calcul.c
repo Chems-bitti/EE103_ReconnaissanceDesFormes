d38c9f8d7f809a05d8f2c5cf821ba1b4c85451fe
Chems-Eddine NAIMI <81744426+Chems-bitti@users.noreply.github.com>
2021-11-29 18:30:51 +0100

    Ajout fonctions calcul des moments geo et centre norme

--- calcul.c
+++ calcul.c
@@ -52,3 +52,66 @@ double base_legendre(int x, int n){
     return res;
 }
 
+Matrice momcentre(BmpImg* pic, int n) {
+	
+	Matrice eta = creerMatrice(1,n); 	// allocation de la matrice
+	Matrice mat = mom(pic, n);		// calcul des moments géometrique
+	
+	double omega = mat.tab[0][0]; 		// Nombre de pixels non nuls
+	double x1 = mat.tab[1][0]/omega;	// x moyen
+	double y1 = mat.tab[0][1]/omega;	// y moyen
+
+	// Boucle sur p et q tq p+q < N
+	for(int p = 0; p < n; p++) {
+		for(int q = 0; q < n-p; q++) {
+			eta.tab[p][q]=0;
+			for(int x = 0; x < pic->dimX; x++){
+				for(int y = 0; y < pic->dimY; y++) {
+					if(getPixel(*pic, x,y) != 0){	//pour recuperer seulement les pixels non nuls;
+						/* Verification si l'exposant de omega est pair ou impair
+						 * si oui : utilisation de l'expression fournie
+						 */
+						if((p+q+2)%2 == 0) {
+							eta.tab[p][q]+= (pow(x-x1,p)*pow(y-y1,q))/pow(abs(omega),(p+q+2)/2.);
+						}
+						/* sinon:
+						 * division par la racine de omega
+						 */
+						else {
+							eta.tab[p][q]+= (pow(x-x1,p)*pow(y-y1,q))/(pow(abs(omega),(p+q+1)/2.)*sqrt(abs(omega)));
+
+						}
+					}
+				}
+			}
+		}
+	}
+	return eta;
+
+}
+
+
+Matrice mom(BmpImg* pic, int n) {
+	Matrice mat = creerMatrice(1,n);
+	/* On calcule les moments pour tout p,q tel que p+q < N
+	 * Alors on aura N lignes
+	 * à chaque ligne i, le nombre de colonnes sera N - i
+	 * On obtiendra alors une matrice triangulaire inversé
+	 */
+	for(int p = 0; p < n; p++) {			
+		for(int q = 0; q < n-p; q++) {			
+			mat.tab[p][q] = 0;				// Initialisation à 0, je fais pas confiance au calloc;
+			// Boucle sur les pixels de l'image
+			for(int x = 0; x < pic->dimX; x++){
+				for(int y = 0; y < pic->dimY; y++) {
+					if(getPixel(*pic, x,y) != 0)	// Vérification que le pixel est non nul
+						mat.tab[p][q] += pow(x,p)*pow(y,q);	// somme
+				}
+				
+
+			}
+		}	
+		
+	}
+	return mat;
+}
