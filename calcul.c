dca8af479ad80719398742386cd7f15b89b2f8ed
Azeyro <62699968+Azeyro@users.noreply.github.com>
2021-11-29 16:55:20 +0100

    Update calcul.c

--- calcul.c
+++ calcul.c
@@ -1,5 +1,25 @@
 #include "calcul.h"
 
+
+double distance_euclidienne(Matrice mat1, Matrice mat2){
+
+    int p,q;
+    double res= 0;
+
+    if (mat1.N != mat2.N){
+        printf("ERREUR : Les matrices ne sont pas de meme ordre\n");
+        return 0;
+    }else{
+        for(p = 0 ; p < mat1.N; p++){
+            for(q = 0 ; q < mat1.N - p;q++){
+                res += pow(mat1.tab[p][q]-mat2.tab[p][q],2);
+            }
+        }
+    }
+    return sqrt(res);
+}
+
+
 double base_legendre(int x, int n){
     double res=0;
     Matrice an = creerMatrice(0,n);
