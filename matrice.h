8f3fce8bbe92c11fd659740c281f4159bed327b0
Duael01 <46139320+Duael01@users.noreply.github.com>
2021-11-29 16:41:02 +0100

    Update matrice.h

--- matrice.h
+++ matrice.h
@@ -5,11 +5,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// déclaration de la structure Matrice
+// dÃ©claration de la structure Matrice
 typedef struct {
-    int num;            // Numéro de la matrice pour l'identifier
+    int num;            // NumÃ©ro de la matrice pour l'identifier
     int N;              // Ordre de la Matrice
-    double **tab        // Tableau avec les valeurs de la matrice
+    double **tab;        // Tableau avec les valeurs de la matrice
 } Matrice;
 
 /** Creation d'une matrice
@@ -25,7 +25,7 @@ Matrice creerMatrice(int num, int N);
 void supprMatrice(Matrice *mat);
 
 /** Affiche le contenu d'une Matrice
-  * Les deux premiers entier sont pour le numéro et l'ordre de la matrice, le tableau en deux dimension pour les valeurs
+  * Les deux premiers entier sont pour le numÃ©ro et l'ordre de la matrice, le tableau en deux dimension pour les valeurs
   * @param mat La matrice a afficher
   */
 void afficheMatrice(Matrice mat);
