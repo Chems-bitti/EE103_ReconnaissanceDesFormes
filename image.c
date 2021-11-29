cea9afbf0613a3b369140e487511017f840100b9
Azeyro <62699968+Azeyro@users.noreply.github.com>
2021-11-29 16:54:55 +0100

    Update image.c

--- image.c
+++ image.c
@@ -16,20 +16,14 @@ unsigned char reconstruction_pixel(int x, int y, Matrice mat){
     return somme2;
 }
 
-double distance_euclidienne(Matrice mat1, Matrice mat2){
 
-    int p,q;
-    double res= 0;
+void reconstruction_image(Matrice mat, BmpImg* pic){
 
-    if (mat1.N != mat2.N){
-        printf("ERREUR : Les matrices ne sont pas de meme ordre\n");
-        return 0;
-    }else{
-        for(p = 0 ; p < mat1.N; p++){
-            for(q = 0 ; q < mat1.N - p;q++){
-                res += pow(mat1.tab[p][q]-mat2.tab[p][q],2);
-            }
-        }
+
+for (int x = 0; x < pic->dimX ; x++){
+    for (int y = 0 ; y < pic->dimY ; y++){
+        pic->img[x][y]=reconstruction_pixel(x,y,mat);
     }
-    return sqrt(res);
 }
+}
+
