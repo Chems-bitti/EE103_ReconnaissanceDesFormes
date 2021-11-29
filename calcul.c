30a2a4e544fa56544a41000c938e1a8c9fe777d0
Duael01 <46139320+Duael01@users.noreply.github.com>
2021-11-29 14:54:42 +0100

    Update calcul.c

--- calcul.c
+++ calcul.c
@@ -1,2 +1,46 @@
 #include "calcul.h"
 
+double base_legendre(int x, int n){
+    double res=0;
+    Matrice an = creerMatrice(0,n);
+
+    an.tab[0][0]=1;
+    if(n>0){
+        an.tab[1][0]=0;
+        an.tab[1][1]=1;
+    }
+    for(int i=2; i<=n; i++){    //Détermination de tous les an jusqu'à l'ordre souhaité
+        for(int j=0; j<=i; j++){
+            if(j==0){
+                an.tab[i][j]=-(i-1)*(an.tab[i-2][j])/i;
+            }
+            else if(j==i){
+                an.tab[i][j]=(2*(i-1)+1)*(an.tab[i-1][i-1])/i;
+            }
+            else{
+                an.tab[i][j]=((2*(i-1)+1)*(an.tab[i-1][j-1])-(an.tab[i-2][j]))/i;
+            }
+        }
+    }
+
+    for(int i=0;i<=n;i++){ //recherche du problème
+        for(int j=0; j<=i; j++){
+            printf("%f ",an.tab[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(int i=0; i<=n; i++){
+        res+=an.tab[n][i]*pow(x,i);
+    }
+
+    supprMatrice(&an);
+
+    for(int i=0;i<=n;i++){ //recherche du problème
+        for(int j=0; j<=i; j++){
+            printf("%f ",an.tab[i][j]);
+        }
+        printf("\n");
+    }
+    return res;
+}
