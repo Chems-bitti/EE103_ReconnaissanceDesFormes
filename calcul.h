dcb54e3c962eef37cc90c5d0e9b3cf5cb140e45d
Chems-Eddine NAIMI <81744426+Chems-bitti@users.noreply.github.com>
2021-11-29 16:54:01 +0100

    Ajout prototypes calcul moments

--- calcul.h
+++ calcul.h
@@ -1,21 +1,37 @@
 #ifndef _calcul_h_
 #define _calcul_h_
 
-// Bibliotheques "standards"
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "myBmpGris.h"
-#include "listeSC.h"
-#include "matrice.h"
+	// Bibliotheques "standards"
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <math.h>
+	#include "myBmpGris.h"
+	#include "listeSC.h"
+	#include "matrice.h"
 
-// Pas teste !!
-/** Calcul la base de Legendre
-  * @param x la valeur souhaité
-  * @param n l'ordre de la base
-  * @return La valeur de x dans la base de Legendre d'ordre n
-  */
-double base_legendre(int x, int n);
+	// Pas teste !!
+	/** Calcul la base de Legendre
+	  * @param x la valeur souhaité
+	  * @param n l'ordre de la base
+	  * @return La valeur de x dans la base de Legendre d'ordre n
+	  */
+	double base_legendre(int x, int n);
+
+	/* Calcul Moments centres normes
+	 * @param BmpImg* pic : Pointeur vers l'image BMP
+	 * @param int n : Ordre des moments
+	 * @return double** eta : Matrice des moments geometriques centres normes
+	 */
+	Matrice momcentre(BmpImg* pic, int n);
+
+	/* Calcul des moments geometriques
+	 * @param BmpImg* pic : Pointeur vers l'image BMP
+	 * @param int n : ordre des moments
+	 * @return double** mat : matrice des moments geometriques
+	 */
+	Matrice mom(BmpImg* pic, int n);
+
+
 
 
 #endif
