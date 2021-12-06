b6f46d7e453d683969ddd621ef0185fa481de8ad
Chems-Eddine NAIMI <81744426+Chems-bitti@users.noreply.github.com>
2021-12-06 13:42:25 +0100

    Update calcul.c
    Modification de fonction base_legendre pour ne pas calculer les coefficients à chaque appel
    Ajout fonction coefLegendre pour calculer les coefficients du polynome de legendre

--- calcul.c
+++ calcul.c
@@ -20,36 +20,31 @@ double distance_euclidienne(Matrice mat1, Matrice mat2){
 }
 
 
-double base_legendre(int x, int n){
+double base_legendre(int x, int n, Matrice coef){
     double res=0;
-    Matrice an = creerMatrice(0,n);
-
-    an.tab[0][0]=1;
-    if(n>0){
-        an.tab[1][0]=0;
-        an.tab[1][1]=1;
-    }
-
-    for(int i=2; i<=n; i++){    //Détermination de tous les an jusqu'à l'ordre souhaité
-        for(int j=0; j<=i; j++){
-            if(j==0){
-                an.tab[i][j]=-(i-1)*(an.tab[i-2][j])/i;
-            }
-            else if(j==i){
-                an.tab[i][j]=(2*(i-1)+1)*(an.tab[i-1][i-1])/i;
-            }
-            else{
-                an.tab[i][j]=((2*(i-1)+1)*(an.tab[i-1][j-1])-(an.tab[i-2][j]))/i;
-            }
-        }
-    }
-
+    x = (double) x;
     for(int i=0; i<=n; i++){
-        res+=(an.tab[n][i])*pow(x,i);
+        res+=(coef.tab[n][i])*pow(x,i);
     }
-    supprMatrice(&an);
-
     return res;
+}
+
+Matrice coefLegendre(int n) {
+	Matrice an = creerMatrice(0, n);
+	an.tab[0][0] = 1;
+	if(n>0){
+        	an.tab[1][0]=0;
+        	an.tab[1][1]=1;
+    	}
+	for(int i=2; i<=n; i++){    //Détermination de tous les an jusqu'à l'ordre souhaité
+        	for(int j=0; j<=i-2; j++){ // on veut pas que j==i
+                	an.tab[i][j]= /*(double)*/ (-i+1)*(an.tab[i-2][j])/ /*(double)*/ i;
+                }
+		for(int j = 0; j <= i; j++) { // rebouclage pour avoir les bons termes
+			an.tab[i][j] += /*(double)*/ (2*i-1)*an.tab[i-1][j-1]/ /*(double)*/ i;
+		}
+    	}
+    return an;
 }
 
 Matrice momcentre(BmpImg* pic, int n) {
