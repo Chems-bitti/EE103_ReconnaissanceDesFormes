071fc3fe2d01e3abebfd75cc4700ff2a9da620c8
Chems-Eddine NAIMI <81744426+Chems-bitti@users.noreply.github.com>
2021-12-08 17:00:45 +0100

    Update gestionfichier.h

--- gestionfichier.h
+++ gestionfichier.h
@@ -10,11 +10,11 @@
   * 1ere ligne : numero de matrice - tab - ordre de la matrice
   * ligne suivant : valeur de la matrice place en triangulaire inf�rieure pour ne pas avoir a marquer les 0
                     chaque valeur est separe par une tabulation
-  * @param nomfichier Le nom du fichier dans lequel ecrire la matrice
+  * @param fname Le nom du fichier dans lequel ecrire la matrice
   * @param mat La matrice qui sera ecrite
   * @return 1 si le fichier s'est bien ouvert, sinon 0
   */
-int ecritureMatrice(char * nomfichier, Matrice mat);
+int ecritureMatrice(char* fname, Matrice mat);
 
 // Pas creer !!
 /** Recupere une matrice dans un fichier texte
