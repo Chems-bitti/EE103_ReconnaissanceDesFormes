c80fccbe39a828db436483a44d4e0dfb465b7f52
Benjyapa <94632472+Benjyapa@users.noreply.github.com>
2021-12-09 11:58:43 +0100

    Update base2donnee.h

--- base2donnee.h
+++ base2donnee.h
@@ -9,21 +9,21 @@
 #include "image.h"
 #include "listeSC.h"
 
-#define taillenom       20
-#define tailledescri    60
+#define taillenom 20
+#define taillenomfichier 60
+
 
 // déclaration de la structure ImageBD
 typedef struct {
+    char* nom;              // Nom de ce qu'il y a a l'image
     char* nomfimage;        // Nom du fichier image
     char* nomfmatrice;      // Nom du fichier de la matrice des moments de Legendre
-    char* description;      // Description dans le cas ou le nom n'est pas explicite
     int N;                  // Ordre de la matrice de l'image
 } ImageBD;
 
 // déclaration de la structure BD
 typedef struct {
     char* nom;              // Nom de la base de donnee
-    char* description;      // Description dans le cas ou le nom n'est pas explicite
     int nbimage;            // Nombre d'image dans la base de donnee
     ListeSC* listeimage;    // Liste chaine des image dans la base de donnee
 } BaseDonnee;
@@ -35,14 +35,12 @@ typedef struct {
   * @param bd :             Base de donnee dans laquelle on veut ajouter l'image
   * @param nomfimage :      Nom du fichier image
   * @param nomfmatrice :    Nom du fichier de la matrice
-  * @param description :    Description de l'image
   * @param N :              Ordre de la matrice de l'image
   */
-void ajoutBD(BaseDonnee bd, char* nomfimage, char* nomfmatrice, char* description, int* N);
+void ajoutBD(BaseDonnee bd, char* nomfimage, char* nomfmatrice, int* N);
 
-
-BaseDonnee* creerBD(char* nom, char* description);
-ImageBD* creerImageBD(char* nomfimage, char* nomfmatrice, char* description);
+BaseDonnee* creerBD(char* nom);
+ImageBD* creerImageBD(char* nomfimage, char* nomfmatrice);
 void ajoutImageBD(BaseDonnee* bd, ImageBD* imgbd);
 void suprimeBD(BaseDonnee* bd);
 void suprimeImageBD(ImageBD* imgbd);
