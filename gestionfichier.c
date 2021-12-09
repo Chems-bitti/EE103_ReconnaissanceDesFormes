9ed2a74262359b8977255473f5c720b840f21dd8
Benjyapa <94632472+Benjyapa@users.noreply.github.com>
2021-12-09 11:41:25 +0100

    Update gestionfichier.c

--- gestionfichier.c
+++ gestionfichier.c
@@ -46,11 +46,10 @@ int ecritureMatricebin(char *nomfichierbin, Matrice mat){
         return 0;
     }else{
 
-        fwrite ( &mat.num , sizeof (int) , 1 , fbin );                              // Ecrit le numero de la matrice
         fwrite ( &mat.N , sizeof (int) , 1 , fbin );                                // Ecrit l'ordre de la matrice
 
+	/*
         int q;
-        /*/
         for (q = 0; q <=mat.N; q++){
             fwrite ( &mat.tab[q] , sizeof (double) , mat.N+1 , fbin );      // Ecrit la matrice ligne par ligne
         } //*/
@@ -72,15 +71,14 @@ Matrice lectureMatricebin(char *nomfichierbin){
         return creerMatrice(0,0);
     }else{
 
-        int num, N;
+        int N;
 
-        fread ( &num , sizeof (int) , 1 , fbin );                  // Ecrit le numero de la matrice
         fread ( &N , sizeof (int) , 1 , fbin );                    // Ecrit l'ordre de la matrice
 
-        Matrice mat = creerMatrice(num,N);
-
+        Matrice mat = creerMatrice(N);
+	
+	/*
         int q;
-        /*
         for (q = 0; q <=mat.N; q++){
             fread ( &mat.tab[q] , sizeof (double) , mat.N+1 , fbin );    // Lit la matrice ligne par ligne
         } //*/
