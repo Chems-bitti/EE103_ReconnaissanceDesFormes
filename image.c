a3a30b6e5ae0d4d8037882268e6e773436bd1a88
Duael01 <46139320+Duael01@users.noreply.github.com>
2021-12-13 16:36:55 +0100

    Update image.c
    modif commentaires

--- image.c
+++ image.c
@@ -16,9 +16,9 @@ unsigned char reconstruction_pixel(int x, int y, Matrice mat, Matrice coef, int 
         }
 
     }
-    if(somme>1) somme = 1; // pour ne pas overflow?
-    if(somme < 0) somme = 0; // pour ne pas underflow?
-    // de toute façon on veut pas dépasser 255 et on ne veut pas être négatif
+    if(somme>1) somme = 1; // pour ne pas overflow
+    if(somme < 0) somme = 0; // pour ne pas underflow
+    
     return (unsigned char) (somme*255);
 
 }
