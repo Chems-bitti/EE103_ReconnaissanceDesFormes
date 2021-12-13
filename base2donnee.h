97e4cf3e8104502ef7cf00f5db053d1937c04b60
Azeyro <62699968+Azeyro@users.noreply.github.com>
2021-12-13 16:11:46 +0100

    Update base2donnee.h

--- base2donnee.h
+++ base2donnee.h
@@ -28,17 +28,70 @@ typedef struct {
 } BaseDonnee;
 
 
-
+// Marche nickel
+/** Reconstruit un pixel de coordonnee (x,y) à partir d'une Matrice de moment de Legendre
+  * @param x coordonnee en abscisse
+  * @param y coordonnee en ordonnee
+  * @param mat Matrice de moment de Legendre
+  * @param Matrice coef : coefficients du polynome de legendre à passer à la fonction baseLegendre
+  * @param int dimX, int dimY : dimensions de notre image
+  * @return La valeur du pixel
+  */
 BaseDonnee* creerBD(char* nom);
+
+
+
+// Marche nickel
+/** Reconstruit un pixel de coordonnee (x,y) à partir d'une Matrice de moment de Legendre
+  * @param x coordonnee en abscisse
+  * @param y coordonnee en ordonnee
+  * @param mat Matrice de moment de Legendre
+  * @param Matrice coef : coefficients du polynome de legendre à passer à la fonction baseLegendre
+  * @param int dimX, int dimY : dimensions de notre image
+  * @return La valeur du pixel
+  */
 ImageBD* creerImageBD(char* nomfimage, char* nomfmatrice);
+
+
+
+// Marche nickel
+/** Reconstruit un pixel de coordonnee (x,y) à partir d'une Matrice de moment de Legendre
+  * @param x coordonnee en abscisse
+  * @param y coordonnee en ordonnee
+  * @param mat Matrice de moment de Legendre
+  * @param Matrice coef : coefficients du polynome de legendre à passer à la fonction baseLegendre
+  * @param int dimX, int dimY : dimensions de notre image
+  * @return La valeur du pixel
+  */
 void ajoutImageBD(BaseDonnee* bd, ImageBD* imgbd);
+
+
+
+
+// Marche nickel
+/** Reconstruit un pixel de coordonnee (x,y) à partir d'une Matrice de moment de Legendre
+  * @param x coordonnee en abscisse
+  * @param y coordonnee en ordonnee
+  * @param mat Matrice de moment de Legendre
+  * @param Matrice coef : coefficients du polynome de legendre à passer à la fonction baseLegendre
+  * @param int dimX, int dimY : dimensions de notre image
+  * @return La valeur du pixel
+  */
 void suprimeBD(BaseDonnee* bd);
+
+
+
+
+// Marche nickel
+/** Reconstruit un pixel de coordonnee (x,y) à partir d'une Matrice de moment de Legendre
+  * @param x coordonnee en abscisse
+  * @param y coordonnee en ordonnee
+  * @param mat Matrice de moment de Legendre
+  * @param Matrice coef : coefficients du polynome de legendre à passer à la fonction baseLegendre
+  * @param int dimX, int dimY : dimensions de notre image
+  * @return La valeur du pixel
+  */
 void suprimeImageBD(ImageBD* imgbd);
-
-
-
-
-
 
 
 #endif
