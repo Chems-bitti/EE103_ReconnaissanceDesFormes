13668ed64c845aff1c347bd31852c3f18d4181d3
Azeyro <62699968+Azeyro@users.noreply.github.com>
2021-12-13 16:08:28 +0100

    Update base2donnee.h

--- base2donnee.h
+++ base2donnee.h
@@ -12,7 +12,7 @@
 #define taillenomfichier 60
 
 
-// déclaration de la structure ImageBD
+// dÃ©claration de la structure ImageBD
 typedef struct {
     char* nom;              // Nom de ce qu'il y a a l'image
     char* nomfimage;        // Nom du fichier image
@@ -20,23 +20,14 @@ typedef struct {
     int N;                  // Ordre de la matrice de l'image
 } ImageBD;
 
-// déclaration de la structure BD
+// dÃ©claration de la structure BD
 typedef struct {
     char* nom;              // Nom de la base de donnee
     int nbimage;            // Nombre d'image dans la base de donnee
     ListeSC* listeimage;    // Liste chaine des image dans la base de donnee
 } BaseDonnee;
 
-// Pas cree !!
-/** Ajoute des matrices d'une image a la base de donnee
-  * Chaque matrice ajoute a ajoute a la liste des matrices de l'image
-  * Si l'image n'est pas dans listeimage, elle est ajoute
-  * @param bd :             Base de donnee dans laquelle on veut ajouter l'image
-  * @param nomfimage :      Nom du fichier image
-  * @param nomfmatrice :    Nom du fichier de la matrice
-  * @param N :              Ordre de la matrice de l'image
-  */
-void ajoutBD(BaseDonnee bd, char* nomfimage, char* nomfmatrice, int* N);
+
 
 BaseDonnee* creerBD(char* nom);
 ImageBD* creerImageBD(char* nomfimage, char* nomfmatrice);
